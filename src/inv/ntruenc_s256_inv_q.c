dbcbeb170868ceac8467bc997d72cd1f91051f01
Sean Parkinson <sparkinson@secure-crypto.org>
2016-05-29 14:49:52 +1000

    Make inversion constant time

--- src/inv/ntruenc_s256_inv_q.c
+++ src/inv/ntruenc_s256_inv_q.c
@@ -22,6 +22,7 @@
 
 #include <string.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "ntruenc_lcl.h"
 
 static uint32_t ntruenc_inv[] = {
@@ -996,11 +997,9 @@ typedef struct ntruenc_mod_inv_st
     int16_t bd[2*NTRU_S256_N+1];
     /* The pointer to the start of the inverse value data. */
     int16_t *b;
-    /* The index of the end of the inverse value. */
-    int16_t be;
 
     /* The data for the f value. */
-    int16_t fd[NTRU_S256_N+1];
+    int16_t fd[2*NTRU_S256_N+1];
     /* The pointer to start of the f value data. */
     int16_t *f;
     /* The index of the start of the f value. */
@@ -1013,20 +1012,17 @@ int ntruenc_s256_mod_inv_q(short *r, short *a)
     NTRUENC_MOD_INV mi[2];
     NTRUENC_MOD_INV *m0 = &mi[0];
     NTRUENC_MOD_INV *m1 = &mi[1];
-    NTRUENC_MOD_INV *tm;
     uint32_t imq;
 
     /* Set first inverse value to: 1 */
     m0->b = &m0->bd[NTRU_S256_N];
-    memset(m0->bd, 0, sizeof(*m0->bd)*NTRU_S256_N);
+    memset(m0->bd, 0, sizeof(m0->bd));
     m0->b[0] = 1;
-    m0->be = 0;
 
     /* Set second inverse value to: 0 */
     m1->b = &m1->bd[NTRU_S256_N];
-    memset(m1->bd, 0, sizeof(*m1->bd)*NTRU_S256_N);
+    memset(m1->bd, 0, sizeof(m1->bd));
     m1->b[0] = 0;
-    m1->be = 0;
 
     /* Set first f value to input */
     m0->f = &m0->fd[0];
@@ -1034,6 +1030,7 @@ int ntruenc_s256_mod_inv_q(short *r, short *a)
     for (i=0; i<NTRU_S256_N; i++)
         m0->f[i+1] = (NTRU_S256_Q + a[i]) % NTRU_S256_Q;
     m0->fs = 1;
+    memset(&m0->f[NTRU_S256_N+1], 0, NTRU_S256_N*sizeof(*m0->fd));
 
     /* Set second value to: 0 (-1,...,1) */
     m1->f = &m1->fd[0];
@@ -1042,96 +1039,41 @@ int ntruenc_s256_mod_inv_q(short *r, short *a)
     m1->f[NTRU_S256_N] = 1;
     m1->fs = 0;
 
-    k = 0;
-    while (1)
+    for (k=0; k<NTRU_S256_N*2-1; k++)
     {
         /* Find first non-zero element. */
-        for (i=m0->fs; i<=NTRU_S256_N && m0->f[i] == 0; i++) ;
-        if (i == NTRU_S256_N+1)
-            return NTRU_ERR_NO_INVERSE;
+        for (d=0,i=0; i<=NTRU_S256_N; i++)
+            d |= (0 - ((d == 0) && (m0->f[m0->fs+i] != 0))) & (i+1);
+        d -= d > 0;
 
-        d = i - m0->fs;
-        if (d > 0)
-        {
-            k += d;
-            /* Move start position to index of first non-zero element. */
-            m0->fs = i;
-            /* Prepend other b a number of zeros. */
-            m1->b -= d;
-            /* Update end index. */
-            m1->be += d;
-        }
-
-        /* Check whether we have one element of 1 - finished. */
-        if ((m0->fs == NTRU_S256_N) && (m0->f[NTRU_S256_N] == 1))
-            break;
+        /* Move start position to index of first non-zero element. */
+        m0->fs += d;
+        /* Prepend other b a number of zeros. */
+        m1->b -= d;
 
         /* Make current index be the longer of the two - start is smaller. */
-        if (m0->fs > m1->fs)
-        {
-            tm = m0;
-            m0 = m1;
-            m1 = tm;
-        }
+        m0 = &mi[mi[0].fs > mi[1].fs];
+        m1 = &mi[mi[0].fs <= mi[1].fs];
 
-        if (m0->fs < NTRU_S256_N)
-            imq = (ntruenc_inv[m1->f[m1->fs]] * (NTRU_S256_Q - m0->f[m0->fs])) % NTRU_S256_Q;
-        else
-            imq = (ntruenc_inv[m1->f[m1->fs]] * (NTRU_S256_Q - m0->f[m0->fs] + 1)) % NTRU_S256_Q;
+        imq = (ntruenc_inv[m1->f[m1->fs]] * (NTRU_S256_Q - m0->f[m0->fs] +
+            (m0->fs == NTRU_S256_N))) % NTRU_S256_Q;
 
         /* Add the two. */
-        for (i=m1->fs,j=m0->fs; i<=NTRU_S256_N; i++,j++)
-            m0->f[j] = (m0->f[j] + imq * m1->f[i]) % NTRU_S256_Q;
-
-        /* If the same length then we may have zero(s) at the end. */
-        if ((m0->fs == m1->fs) && (m0->f[NTRU_S256_N] == 0))
-        {
-            /* Find last non-zero element. */
-            for (i=NTRU_S256_N-1; i>=m0->fs && m0->f[i] == 0; i--) ;
-            if (i < m0->fs)
-                return NTRU_ERR_NO_INVERSE;
-            /* Move pointer back and start index forward so last element's
-             * index is at NTRU_S256_N.
-             */
-            d = NTRU_S256_N - i;
-            m0->f -= d;
-            m0->fs += d;
-        }
-
-        /* Append zeros to ensure the first is as long as the second. */
-        for (i=m0->be+1; i<=m1->be; i++)
-            m0->b[i] = 0;
-        /* Update end index. */
-        m0->be = i-1;
+        for (i=0; i<=NTRU_S256_N; i++)
+            m0->f[m0->fs+i] = (m0->f[m0->fs+i] + imq * m1->f[m1->fs+i]) % NTRU_S256_Q;
 
         /* Add the two. */
-        for (i=0; i<=m1->be; i++)
+        for (i=0; i<=NTRU_S256_N; i++)
             m0->b[i] = (m0->b[i] + imq * m1->b[i]) % NTRU_S256_Q;
-
-        /* If the same length then we may have zero(s) at the end. */
-        if ((m0->be == m1->be) && (m0->b[m0->be] == 0))
-        {
-            /* Find last non-zero element. */
-            for (i=m0->be-1; i>=0 && m0->b[i] == 0; i--) ;
-            m0->be = i+1;
-        }
     }
-
-    /* Zero out the rest of the vector. */
-    for (i=m0->be+1; i<=NTRU_S256_N; i++)
-        m0->b[i] = 0;
-    /* The start of the result is: 0..NTRU_S256_N-1 */
-    if (k > NTRU_S256_N) k -= NTRU_S256_N;
 
     /* Move the values in but wrt the fact that there are NTRU_S256_N+1
      * values.
      */
-    for (i=k,j=0; i<NTRU_S256_N; i++,j++)
-        r[j] = m0->b[i];
-    r[j] = (m0->b[0] + m0->b[NTRU_S256_N]) % NTRU_S256_Q;
-    j++;
-    for (i=1; i<k; i++,j++)
+    r[0] = m0->b[NTRU_S256_N-1];
+    r[1] = (m0->b[0] + m0->b[NTRU_S256_N]) % NTRU_S256_Q;
+    for (i=1,j=2; i<k; i++,j++)
         r[j] = m0->b[i];
 
-    return 0;
+    return NTRU_ERR_NO_INVERSE & (0 - (m0->f[m0->fs] != 1));
 }
